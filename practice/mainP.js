17ceebee315dafcaf112446af39b89d341d7e76f
Phamlamthai <105089922+Phamlamthai@users.noreply.github.com>
2024-02-19 11:46:40 +0700

    modified

--- practice/mainP.js
+++ practice/mainP.js
@@ -44,7 +44,7 @@ function render(arr) {
   });
 }
 
-function getDataPromise2(url2) {
+function getDataPromise2(url) {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
     xhr.onload = function () {
@@ -55,23 +55,22 @@ function getDataPromise2(url2) {
         reject(new Error(xhr.status));
       }
     };
-    xhr.open("GET", url2);
+    xhr.open("GET", url);
     xhr.send();
   });
 }
-
 let p = getDataPromise2(USER_URL);
 let users = [];
 p.then((userResponse) => {
   users = userResponse;
   console.log(users);
-
   return Promise.all(
-    userResponse.map((user) => getDataPromise2(`${USER_URL}/${user.id}/posts`))
+    userResponse.map((user, i) =>
+      getDataPromise2(`${USER_URL}/${user.id}/posts`)
+    )
   );
 })
   .then((allPosts) => {
-    // console.log(allPosts);
     users.forEach((user, index) => (user.posts = allPosts[index]));
     return Promise.all(
       allPosts.map((posts) =>
@@ -87,11 +86,62 @@ p.then((userResponse) => {
     allComments.forEach((user, i) => {
       user.forEach((comments, j) => {
         users[i].posts[j].comments = comments;
-        // console.log(users);
       });
     });
     render(users);
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.log(error);
   });
+
+// function getDataPromise2(url2) {
+//   return new Promise((resolve, reject) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.onload = function () {
+//       if (xhr.status === 200) {
+//         let data = JSON.parse(xhr.responseText);
+//         resolve(data);
+//       } else {
+//         reject(new Error(xhr.status));
+//       }
+//     };
+//     xhr.open("GET", url2);
+//     xhr.send();
+//   });
+// }
+
+// let p = getDataPromise2(USER_URL);
+// let users = [];
+// p.then((userResponse) => {
+//   users = userResponse;
+//   console.log(users);
+
+//   return Promise.all(
+//     userResponse.map((user) => getDataPromise2(`${USER_URL}/${user.id}/posts`))
+//   );
+// })
+//   .then((allPosts) => {
+//     // console.log(allPosts);
+//     users.forEach((user, index) => (user.posts = allPosts[index]));
+//     return Promise.all(
+//       allPosts.map((posts) =>
+//         Promise.all(
+//           posts.map((post) =>
+//             getDataPromise2(`${POST_URL}/${post.id}/comments`)
+//           )
+//         )
+//       )
+//     );
+//   })
+//   .then((allComments) => {
+//     allComments.forEach((user, i) => {
+//       user.forEach((comments, j) => {
+//         users[i].posts[j].comments = comments;
+//         // console.log(users);
+//       });
+//     });
+//     render(users);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
